crackersophia/kong

commit 4fd6b18be1dc096abc7d1fa324208dbb1eb1c9d6

Rate-Limiting upgrades and policies (#1432)

kong/plugins/rate-limiting/policies.lua
@@ -1,0 +1,162 @@
+local singletons = require "kong.singletons"
+local timestamp = require "kong.tools.timestamp"
+local cache = require "kong.tools.database_cache"
+local ngx_log = ngx.log
+local ngx_timer_at = ngx.timer.at
+
+local get_local_key = function(api_id, identifier, period_date, period)
+  return string.format("ratelimit:%s:%s:%s:%s", api_id, identifier, period_date, period)
+end
+
+--TODO: Check that it really expires 
+local EXPIRATIONS = {
+  second = 1,
+  minute = 60,
+  hour = 3600,
+  day = 86400,
+  month = 2592000,
+  year = 31104000
+}
+
+return {
+  ["local"] = {
+    increment = function(conf, api_id, identifier, current_timestamp, value)
+      local periods = timestamp.get_timestamps(current_timestamp)
+      local ok = true
+      for period, period_date in pairs(periods) do
+        local cache_key = get_local_key(api_id, identifier, period_date, period)
+        if not cache.rawget(cache_key) then
+          cache.rawset(cache_key, 0, EXPIRATIONS[period])
+        end
+
+        local _, err = cache.incr(cache_key, value)
+        if err then
+          ok = false
+          ngx_log("[rate-limiting] could not increment counter for period '"..period.."': "..tostring(err))
+        end
+      end
+
+      return ok
+    end,
+    usage = function(conf, api_id, identifier, current_timestamp, name)
+      local periods = timestamp.get_timestamps(current_timestamp)
+      local cache_key = get_local_key(api_id, identifier, periods[name], name)
+      local current_metric, err = cache.rawget(cache_key)
+      if err then
+        return nil, err
+      end
+      return current_metric and current_metric or 0
+    end
+  },
+  ["cluster"] = {
+    increment = function(conf, api_id, identifier, current_timestamp, value)
+      local incr = function(premature, api_id, identifier, current_timestamp, value)
+        if premature then return end
+        local _, stmt_err = singletons.dao.ratelimiting_metrics:increment(api_id, identifier, current_timestamp, value)
+        if stmt_err then
+          ngx_log(ngx.ERR, "failed to increment: ", tostring(stmt_err))
+        end
+      end
+
+      local ok, err = ngx_timer_at(0, incr, api_id, identifier, current_timestamp, 1)
+      if not ok then
+        ngx_log(ngx.ERR, "failed to create timer: ", err)
+      end
+    end,
+    usage = function(conf, api_id, identifier, current_timestamp, name)
+      local current_metric, err = singletons.dao.ratelimiting_metrics:find(api_id, identifier, current_timestamp, name)
+      if err then
+        return nil, err
+      end
+      return current_metric and current_metric.value or 0
+    end
+  },
+  ["redis"] = {
+    increment = function(conf, api_id, identifier, current_timestamp, value)
+      local redis = require "resty.redis"
+      local red = redis:new()
+
+      red:set_timeout(conf.redis_timeout)
+      local ok, err = red:connect(conf.redis_host, conf.redis_port)
+      if not ok then
+        ngx_log(ngx.ERR, "failed to connect to Redis: ", err)
+        return
+      end
+
+      if conf.redis_password then
+        local ok, err = red:auth(conf.redis_password)
+        if not ok then
+
+          ngx_log(ngx.ERR, "failed to connect to Redis: ", err)
+          return
+        end
+      end
+
+      local periods = timestamp.get_timestamps(current_timestamp)
+      local ok = true
+      for period, period_date in pairs(periods) do
+        local cache_key = get_local_key(api_id, identifier, period_date, period)
+        local exists, err = red:exists(cache_key)
+        if err then
+          ngx_log(ngx.ERR, "failed to query Redis: ", err)
+          return
+        end
+
+        local ok, err = red:incrby(cache_key, value)
+        if not ok then
+          ngx_log(ngx.ERR, "failed to query Redis: ", err)
+          return
+        end
+
+        if not exists or exists == 0 then
+          local ok, err = red:expire(cache_key, EXPIRATIONS[period])
+          if err then
+            ngx_log(ngx.ERR, "failed to query Redis: ", err)
+            return
+          end
+        end
+      end
+
+      local ok, err = red:set_keepalive(10000, 100)
+      if not ok then
+        ngx_log(ngx.ERR, "failed to set Redis keepalive: ", err)
+        return
+      end
+
+      return true
+    end,
+    usage = function(conf, api_id, identifier, current_timestamp, name)
+      local redis = require "resty.redis"
+      local red = redis:new()
+
+      red:set_timeout(conf.redis_timeout)
+      local ok, err = red:connect(conf.redis_host, conf.redis_port)
+      if not ok then
+        ngx_log(ngx.ERR, "failed to connect to Redis: ", err)
+        return
+      end
+
+      if conf.redis_password then
+        local ok, err = red:auth(conf.redis_password)
+        if not ok then
+
+          ngx_log(ngx.ERR, "failed to connect to Redis: ", err)
+          return
+        end
+      end
+
+      local periods = timestamp.get_timestamps(current_timestamp)
+      local cache_key = get_local_key(api_id, identifier, periods[name], name)
+      local current_metric, err = red:get(cache_key)
+      if err then
+        return nil, err
+      end
+
+      if current_metric == ngx.null then
+        current_metric = nil
+      end
+
+      return current_metric and current_metric or 0
+    end
+  }
+}

kong-0.8.3-0.rockspec
@@ -170,6 +170,7 @@ build = {
     ["kong.plugins.rate-limiting.migrations.postgres"] = "kong/plugins/rate-limiting/migrations/postgres.lua",
     ["kong.plugins.rate-limiting.handler"] = "kong/plugins/rate-limiting/handler.lua",
     ["kong.plugins.rate-limiting.schema"] = "kong/plugins/rate-limiting/schema.lua",
+    ["kong.plugins.rate-limiting.policies"] = "kong/plugins/rate-limiting/policies.lua",
     ["kong.plugins.rate-limiting.dao.cassandra"] = "kong/plugins/rate-limiting/dao/cassandra.lua",
     ["kong.plugins.rate-limiting.dao.postgres"] = "kong/plugins/rate-limiting/dao/postgres.lua",
 

kong/plugins/basic-auth/access.lua
@@ -118,6 +118,7 @@ function _M.execute(conf)
   ngx.req.set_header(constants.HEADERS.CONSUMER_USERNAME, consumer.username)
   ngx.req.set_header(constants.HEADERS.CREDENTIAL_USERNAME, credential.username)
   ngx.ctx.authenticated_credential = credential
+  ngx.ctx.authenticated_consumer = consumer
 end
 
 return _M

kong/plugins/galileo/migrations/cassandra.lua
@@ -21,6 +21,7 @@ return {
         local _, err = dao.plugins:insert {
           name = "galileo",
           api_id = analytics.api_id,
+          consumer_id = analytics.consumer_id,
           enabled = analytics.enabled,
           config = {
             service_token = analytics.config.service_token,

kong/plugins/galileo/migrations/postgres.lua
@@ -21,6 +21,7 @@ return {
         local _, err = dao.plugins:insert {
           name = "galileo",
           api_id = analytics.api_id,
+          consumer_id = analytics.consumer_id,
           enabled = analytics.enabled,
           config = {
             service_token = analytics.config.service_token,

kong/plugins/hmac-auth/access.lua
@@ -184,6 +184,7 @@ function _M.execute(conf)
   ngx_set_header(constants.HEADERS.CONSUMER_USERNAME, consumer.username)
   ngx.req.set_header(constants.HEADERS.CREDENTIAL_USERNAME, credential.username)
   ngx.ctx.authenticated_credential = credential
+  ngx.ctx.authenticated_consumer = consumer
 end
 
 return _M

kong/plugins/jwt/handler.lua
@@ -139,6 +139,7 @@ function JwtHandler:access(conf)
   ngx.req.set_header(constants.HEADERS.CONSUMER_CUSTOM_ID, consumer.custom_id)
   ngx.req.set_header(constants.HEADERS.CONSUMER_USERNAME, consumer.username)
   ngx.ctx.authenticated_credential = jwt_secret
+  ngx.ctx.authenticated_consumer = consumer
 end
 
 return JwtHandler

commit bc0e15e96e9b0c6e078af939516e869aefd53868

fix(lint) linting broken by #1432

kong/plugins/rate-limiting/policies.lua
@@ -8,7 +8,7 @@ local get_local_key = function(api_id, identifier, period_date, period)
   return string.format("ratelimit:%s:%s:%s:%s", api_id, identifier, period_date, period)
 end
 
---TODO: Check that it really expires 
+--TODO: Check that it really expires
 local EXPIRATIONS = {
   second = 1,
   minute = 60,
@@ -93,7 +93,6 @@ return {
       end
 
       local periods = timestamp.get_timestamps(current_timestamp)
-      local ok = true
       for period, period_date in pairs(periods) do
         local cache_key = get_local_key(api_id, identifier, period_date, period)
         local exists, err = red:exists(cache_key)
@@ -109,7 +108,7 @@ return {
         end
 
         if not exists or exists == 0 then
-          local ok, err = red:expire(cache_key, EXPIRATIONS[period])
+          local _, err = red:expire(cache_key, EXPIRATIONS[period])
           if err then
             ngx_log(ngx.ERR, "failed to query Redis: ", err)
             return

spec/02-integration/07-cache/01-database_cache_spec.lua
@@ -36,7 +36,7 @@ describe("Resolver", function()
   it("avoids dog-pile effect", function()
     local function make_request(premature, sleep_time)
       local client = helpers.proxy_client()
-      local res = assert(client:send {
+      assert(client:send {
         method = "GET",
         path = "/status/200?sleep="..sleep_time,
         headers = {

spec/03-plugins/98-rate-limiting/04-access_spec.lua
@@ -53,7 +53,14 @@ for i, policy in ipairs({"local", "cluster", "redis"}) do
       assert(helpers.dao.plugins:insert {
         name = "rate-limiting",
         api_id = api1.id,
-        config = { policy = policy, minute = 6, cluster_fault_tolerant = false, redis_host = REDIS_HOST, redis_port = REDIS_PORT, redis_password = REDIS_PASSWORD }
+        config = {
+          policy = policy,
+          minute = 6,
+          cluster_fault_tolerant = false,
+          redis_host = REDIS_HOST,
+          redis_port = REDIS_PORT,
+          redis_password = REDIS_PASSWORD
+        }
       })
 
       local api2 = assert(helpers.dao.apis:insert {
@@ -63,7 +70,15 @@ for i, policy in ipairs({"local", "cluster", "redis"}) do
       assert(helpers.dao.plugins:insert {
         name = "rate-limiting",
         api_id = api2.id,
-        config = { minute = 3, hour = 5, cluster_fault_tolerant = false, policy = policy, redis_host = REDIS_HOST, redis_port = REDIS_PORT, redis_password = REDIS_PASSWORD }
+        config = {
+          minute = 3,
+          hour = 5,
+          cluster_fault_tolerant = false,
+          policy = policy,
+          redis_host = REDIS_HOST,
+          redis_port = REDIS_PORT,
+          redis_password = REDIS_PASSWORD
+        }
       })
 
       local api3 = assert(helpers.dao.apis:insert {
@@ -77,13 +92,28 @@ for i, policy in ipairs({"local", "cluster", "redis"}) do
       assert(helpers.dao.plugins:insert {
         name = "rate-limiting",
         api_id = api3.id,
-        config = { minute = 6, limit_by = "credential", cluster_fault_tolerant = false, policy = policy, redis_host = REDIS_HOST, redis_port = REDIS_PORT, redis_password = REDIS_PASSWORD }
+        config = {
+          minute = 6,
+          limit_by = "credential",
+          cluster_fault_tolerant = false,
+          policy = policy,
+          redis_host = REDIS_HOST,
+          redis_port = REDIS_PORT,
+          redis_password = REDIS_PASSWORD
+        }
       })
       assert(helpers.dao.plugins:insert {
         name = "rate-limiting",
         api_id = api3.id,
         consumer_id = consumer1.id,
-        config = { minute = 8, cluster_fault_tolerant = false, policy = policy, redis_host = REDIS_HOST, redis_port = REDIS_PORT, redis_password = REDIS_PASSWORD }
+        config = {
+          minute = 8,
+          cluster_fault_tolerant = false,
+          policy = policy,
+          redis_host = REDIS_HOST,
+          redis_port = REDIS_PORT,
+          redis_password = REDIS_PASSWORD
+        }
       })
 
       local api4 = assert(helpers.dao.apis:insert {
@@ -98,7 +128,14 @@ for i, policy in ipairs({"local", "cluster", "redis"}) do
         name = "rate-limiting",
         api_id = api4.id,
         consumer_id = consumer1.id,
-        config = { minute = 6, cluster_fault_tolerant = true, policy = policy, redis_host = REDIS_HOST, redis_port = REDIS_PORT, redis_password = REDIS_PASSWORD }
+        config = {
+          minute = 6,
+          cluster_fault_tolerant = true,
+          policy = policy,
+          redis_host = REDIS_HOST,
+          redis_port = REDIS_PORT,
+          redis_password = REDIS_PASSWORD
+        }
       })
     end)
     teardown(function()
@@ -374,9 +411,10 @@ for i, policy in ipairs({"local", "cluster", "redis"}) do
       describe("Expirations", function()
         local api
         setup(function()
-          helpers.kill_all()
+          helpers.stop_kong()
           helpers.dao:drop_schema()
           assert(helpers.dao:run_migrations())
+          assert(helpers.start_kong())
 
           api = assert(helpers.dao.apis:insert {
             request_host = "expire1.com",
@@ -385,14 +423,19 @@ for i, policy in ipairs({"local", "cluster", "redis"}) do
           assert(helpers.dao.plugins:insert {
             name = "rate-limiting",
             api_id = api.id,
-            config = { second = 6, policy = policy, redis_host = REDIS_HOST, redis_port = REDIS_PORT, redis_password = REDIS_PASSWORD, cluster_fault_tolerant = false }
+            config = {
+              second = 6,
+              policy = policy,
+              redis_host = REDIS_HOST,
+              redis_port = REDIS_PORT,
+              redis_password = REDIS_PASSWORD,
+              cluster_fault_tolerant = false
+            }
           })
-
-          assert(helpers.start_kong())
         end)
 
         it("expires a counter", function()
-          local periods = timestamp.get_timestamps(current_timestamp)
+          local periods = timestamp.get_timestamps()
 
           local res = assert(helpers.proxy_client():send {
             method = "GET",

spec/helpers.lua
@@ -182,7 +182,7 @@ end
 --- returns a pre-configured `http_client` for the Kong proxy port.
 -- @name proxy_client
 local function proxy_client(timeout)
-  return http_client(conf.proxy_ip, conf.proxy_port, tuimeout)
+  return http_client(conf.proxy_ip, conf.proxy_port, timeout)
 end
 
 --- returns a pre-configured `http_client` for the Kong SSL proxy port.