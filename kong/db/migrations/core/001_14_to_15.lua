Kong/kong-test-new-ci

feat(migrations) add path_handling with smart defaults
If migrating from 0.14.1, initialize `path_handling` with `'v0'`.

If migrating from 1.x, create `path_handling` it with a default of `'v1'` to
fill existing rows, then change the default.

kong/db/migrations/core/001_14_to_15.lua
@@ -249,6 +249,16 @@ return {
         "key"   TEXT     NOT NULL,
         "cert"  TEXT     NOT NULL
       );
+
+
+      -- Migrating from 0.14.1 into 2.0: add path_handling with v0 as default
+      DO $$
+      BEGIN
+        ALTER TABLE IF EXISTS ONLY "routes" ADD "path_handling" TEXT DEFAULT 'v0';
+      EXCEPTION WHEN DUPLICATE_COLUMN THEN
+        -- Do nothing, accept existing state
+      END;
+      $$;
     ]],
 
     teardown = function(connector, helpers)
@@ -311,9 +321,25 @@ return {
         key text,
         cert text
       );
+
+      ALTER TABLE plugins ADD path_handling text;
     ]],
 
     teardown = function(connector, helpers)
+      local coordinator = assert(connector:connect_migrations())
+
+      for rows, err in coordinator:iterate([[SELECT * FROM routes]]) do
+        if err then
+          return nil, err
+        end
+
+        for _, row in ipairs(rows) do
+          assert(connector:query([[
+            UPDATE routes SET path_handling = 'v0'
+            WHERE partition = 'routes' AND id = ]] .. row.id))
+        end
+      end
+
       local plugins_def = {
         name = "plugins",
         columns = {

crackersophia/kong

kong/db/migrations/core/001_14_to_15.lua
@@ -249,6 +249,16 @@ return {
         "key"   TEXT     NOT NULL,
         "cert"  TEXT     NOT NULL
       );
+
+
+      -- Migrating from 0.14.1 into 2.0: add path_handling with v0 as default
+      DO $$
+      BEGIN
+        ALTER TABLE IF EXISTS ONLY "routes" ADD "path_handling" TEXT DEFAULT 'v0';
+      EXCEPTION WHEN DUPLICATE_COLUMN THEN
+        -- Do nothing, accept existing state
+      END;
+      $$;
     ]],
 
     teardown = function(connector, helpers)
@@ -311,9 +321,25 @@ return {
         key text,
         cert text
       );
+
+      ALTER TABLE routes ADD path_handling text;
     ]],
 
     teardown = function(connector, helpers)
+      local coordinator = assert(connector:connect_migrations())
+
+      for rows, err in coordinator:iterate([[SELECT * FROM routes]]) do
+        if err then
+          return nil, err
+        end
+
+        for _, row in ipairs(rows) do
+          assert(connector:query([[
+            UPDATE routes SET path_handling = 'v0'
+            WHERE partition = 'routes' AND id = ]] .. row.id))
+        end
+      end
+
       local plugins_def = {
         name = "plugins",
         columns = {